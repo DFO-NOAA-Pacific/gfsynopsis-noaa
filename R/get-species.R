f4a23dd837d538f01c06556209b9f56c5a6b60d8
Chantel Wetzel <chantel.wetzel@noaa.gov>
2024-07-02 11:21:19 -0700

    list of species to pull data for

---
+++ R/get-species.R
@@ -0,0 +1,10 @@
+# List of species to pull and estimates for.
+# If adding new species to the list these species will also need to be added
+# to the nwfsc_strata_by_species() with any special stratas also added to
+# get_nwfsc_strata().
+
+spp_list <- list(
+  "Pacific ocean perch",
+  "Dover sole",
+  "arrowtooth flounder",
+  "sablefish")
