e06cfb59252a36fafd4109b9769413bf343e91ec
Chantel Wetzel <chantel.wetzel@noaa.gov>
2024-10-28 15:08:08 -0700

    add html example

---
+++ doc/to_plot.R
@@ -0,0 +1,38 @@
+to_plot <- function(
+  common_name,
+  biomass,
+  specimens,
+  parameters,
+  samples = NULL) {
+
+  firstup <- function(x) {
+    substr(x, 1, 1) <- toupper(substr(x, 1, 1))
+    x
+  }
+
+  section_name <- firstup(common_name)
+  glue::glue(" \n{section_name} \n \n") |> cat( )
+  #glue::glue(" \n# {section_name} {{-}}\n \n") |> cat( )
+
+  bio_plot <- plot_design_based(
+    data = biomass[biomass$common_name == common_name, ],
+    plot = 1)
+
+  mass_length <- plot_biology(
+    data = specimens[specimens$common_name == common_name, ],
+    parameters = parameters,
+    plot = 1)
+
+  length_age <- plot_biology(
+    data = specimens[specimens$common_name == common_name, ],
+    parameters = parameters,
+    plot = 2)
+
+  suppressWarnings(print(bio_plot))
+  suppressWarnings(print(mass_length))
+  suppressWarnings(print(length_age))
+  #cowplot::plot_grid(
+  #  mass_length, length_age, ncol = 1, nrow = 2,
+  #  rel_widths = 1.2, rel_heights = 1.2
+  #)
+}
